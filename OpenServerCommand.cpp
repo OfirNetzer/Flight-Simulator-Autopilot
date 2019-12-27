6c80e31f8df9b8c17a13b0e32a01fa6b8f5bab04
avichaigel <59146878+avichaigel@users.noreply.github.com>
2019-12-27 15:43:46 +0200

    Add files via upload

--- OpenServerCommand.cpp
+++ OpenServerCommand.cpp
@@ -15,12 +15,12 @@
 
 using namespace std;
 
-int OpenServerCommand::execute(vector<string> arr, int ind) {{
+int OpenServerCommand::execute(vector<string> arr, int ind) {
     //create socket
     int socketfd = socket(AF_INET, SOCK_STREAM, 0);
     if (socketfd == -1) {
         //error
-        std::cerr << "Could not create a socket"<<std::endl;
+        cerr << "Could not create a socket"<<endl;
         return -1;
     }
 
@@ -34,16 +34,16 @@ int OpenServerCommand::execute(vector<string> arr, int ind) {{
 
     //the actual bind command
     if (bind(socketfd, (struct sockaddr *) &address, sizeof(address)) == -1) {
-        std::cerr<<"Could not bind the socket to an IP"<<std::endl;
+        cerr<<"Could not bind the socket to an IP"<<endl;
         return -2;
     }
 
     //making socket listen to the port
     if (listen(socketfd, 5) == -1) { //can also set to SOMAXCON (max connections)
-        std::cerr<<"Error during listening command"<<std::endl;
+        cerr<<"Error during listening command"<<endl;
         return -3;
     } else{
-        std::cout<<"Server is now listening ..."<<std::endl;
+        cout<<"Server is now listening ..."<<endl;
     }
 
     // accepting a client
@@ -51,7 +51,7 @@ int OpenServerCommand::execute(vector<string> arr, int ind) {{
                                (socklen_t*)&address);
 
     if (client_socket == -1) {
-        std::cerr<<"Error accepting client"<<std::endl;
+        cerr<<"Error accepting client"<<endl;
         return -4;
     }
 
@@ -60,12 +60,12 @@ int OpenServerCommand::execute(vector<string> arr, int ind) {{
     //reading from client
     char buffer[1024] = {0};
     int valread = read( client_socket , buffer, 1024);
-    std::cout<<buffer<<std::endl;
+    cout<<buffer<<endl;
 
     //writing back to client
-    char *hello = "Hello, I can hear you! \n";
+    char hello[] = "Hello, I can hear you! \n";
     send(client_socket , hello , strlen(hello) , 0 );
-    std::cout<<"Hello message sent\n"<<std::endl;
+    cout<<"Hello message sent\n"<<endl;
     return 0;
 
 }
