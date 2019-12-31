a0fc9b698c580aa78cfaf6adbcd6edd047a1d365
OfirNetzer <59169969+OfirNetzer@users.noreply.github.com>
2019-12-31 14:08:32 +0200

    Add files via upload

---
+++ Parser.h
@@ -0,0 +1,19 @@
+//
+// Created by ofirn93 on 30/12/2019.
+//
+
+#ifndef EX3_PARSER_H
+#define EX3_PARSER_H
+
+#include "symTable.h"
+#include <vector>
+
+
+class Parser {
+
+public:
+    void run(vector<string> lexerCommands);
+};
+
+
+#endif //EX3_PARSER_H
