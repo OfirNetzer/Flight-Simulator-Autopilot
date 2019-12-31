d01c7e1464b848a467b34a7e3eaf71f552ac65e9
Ofir Netzer <ofir.netz@gmail.com>
2019-12-31 20:50:55 +0200

    initial commit

--- Parser.cpp
+++ Parser.cpp
@@ -2,15 +2,45 @@
 // Created by ofirn93 on 30/12/2019.
 //
 
+#include <iostream>
 #include "Parser.h"
+#include "ex1.h"
 
-
+Parser::Parser() {
+    this->ind = 0;
+    OpenServerCommand* osc =new OpenServerCommand();
+    DefineVarCommand* dvc = new DefineVarCommand();
+    connectCommand* cc = new connectCommand();
+    CommandDB::getInstance()->addCommand("openDataServer", osc);
+    CommandDB::getInstance()->addCommand("openDataServer", dvc);
+    CommandDB::getInstance()->addCommand("var", cc);
+    // todo: complete with 1.func 2. while 3.if once we build these commands
+}
 
 void Parser::run(vector <string> lexerCommands) {
 //    CommandDB::getInstance()->getCommand(lexerCommands.at(this->ind));
     while (this->ind < lexerCommands.size()) {
         Command *c = CommandDB::getInstance()->getCommand(lexerCommands.at(this->ind));
         int i = this->ind;
-        this->ind = c->execute(lexerCommands, i);
+        this->ind += c->execute(lexerCommands, this->ind);
+//        this->ind++;
+        /*Interpreter* inter = new Interpreter();
+        Expression* e = nullptr;
+        try {
+            e = inter();
+            delete inter;
+            delete e;
+        } catch (const char* e) {
+            cerr << "Problem with interpreting";
+            if (e != nullptr) {
+                delete e;
+            }
+            if (inter != nullptr) {
+                delete inter;
+            }
+        }*/
     }
 }
+
+
+
--- Parser.h
+++ Parser.h
@@ -17,19 +17,11 @@ using namespace std;
 
 class Parser {
 //    unordered_map<string, Command*> key2Command;
-    int ind = 0;
+    int ind;
 public:
     // func that matches keys (that should come from the lexer) to a command (that we wrote)
     // it is basically initializes the map once the parser is being created
-    Parser() {
-        OpenServerCommand* osc =new OpenServerCommand();
-        DefineVarCommand* dvc = new DefineVarCommand();
-        connectCommand* cc = new connectCommand();
-        CommandDB::getInstance()->addCommand("openDataServer", osc);
-        CommandDB::getInstance()->addCommand("openDataServer", dvc);
-        CommandDB::getInstance()->addCommand("var", cc);
-        // todo: complete with 1.func 2. while 3.if once we build these commands
-    }
+    Parser();
     void run(vector<string> lexerCommands);
 };
 
