9efaf2c1c59cf8409102da652234efec4f9bf37c
OfirNetzer <59169969+OfirNetzer@users.noreply.github.com>
2019-12-31 18:06:04 +0200

    Add files via upload

--- Parser.h
+++ Parser.h
@@ -10,11 +10,24 @@
 #include <unordered_map>
 #include "Command.h"
 #include "CommandDB.h"
+#include "OpenServerCommand.h"
+#include "DefineVarCommand.h"
+#include "connectCommand.h"
 
 
 class Parser {
-    unordered_map<string, Command *> commandMap;
+//    unordered_map<string, Command*> key2Command;
 public:
+    // func that matches keys (that should come from the lexer) to a command (that we wrote)
+    // it is basically initializes the map once the parser is being created
+    Parser() {
+        OpenServerCommand* osc =new OpenServerCommand();
+        DefineVarCommand* dvc = new DefineVarCommand();
+        connectCommand* cc = new connectCommand();
+        CommandDB::getInstance()->addCommand("openDataServer", osc);
+        CommandDB::getInstance()->addCommand("openDataServer", dvc);
+        CommandDB::getInstance()->addCommand("var", cc);
+    }
     void run(vector<string> lexerCommands);
 };
 
